d806d9db7b4c4ac83173a83d74c4fb4abce2e68d
nocat <aabc_1123@qq.com>
2015-10-07 10:32:23 +0800

    2015/10/7

---
+++ mp/BLL/PackageInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mp.DAL;
+
+namespace mp.BLL
+{
+    public class PackageInfo
+    {
+        MiaopassContext _db = new MiaopassContext();
+
+        Package _package = null;
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Uri HomePage { get; set; }
+        public int ID { get; set; }
+
+        int _imageCount=-1;
+        public int ImageCount
+        {
+            get
+            {
+                if(_imageCount==-1)
+                {
+                    _imageCount = _db.Images.Where(i => i.PackageID == _package.ID).Count();
+                }
+                return _imageCount;
+            }
+        }
+
+        ImageInfo _cover = null;
+        public ImageInfo Cover
+        {
+            get
+            {
+                if(_cover==null)
+                {
+                    if (_package.CoverID != 0)
+                    {
+                        var image = _db.Images.Find(_package.CoverID);
+                        _cover = new ImageInfo(image);
+                    }
+                }
+                return _cover;
+            }
+        }
+
+        int _followerCount = -1;
+        public int FollowerCount
+        {
+            get
+            {
+                if(_followerCount==-1)
+                {
+                    _followerCount = _db.Followings.Where(f => f.Type == FollowingTypes.Package && f.Info == _package.ID).Count();
+                }
+                return _followerCount;
+            }
+        }
+
+        UserInfo _user = null;
+        public UserInfo User
+        {
+            get
+            {
+                if (_user==null)
+                {
+                    _user = new UserInfo(_package.User);
+                }
+                return _user;
+            }
+        }
+
+        public PackageInfo(Package package)
+        {
+            _package = package;
+            Title = package.Title;
+            Description = package.Description;
+            HomePage = new Uri("/package/" + package.ID, UriKind.Relative);
+            ID = package.ID;
+        }
+    }
+}
---
+++ mp/Controllers/ControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mp.DAL;
+using System.Web.Mvc;
+
+namespace mp.Controllers
+{
+    public class ControllerBase : Controller
+    {
+        MiaopassContext _db = new MiaopassContext();
+        public MiaopassContext DB
+        {
+            get
+            {
+                return _db;
+            }
+        }
+    }
+}
