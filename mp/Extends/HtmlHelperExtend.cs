c9ce8c127cd5945f4ce548a91937d897465cc3e2
nocat <aabc_1123@qq.com>
2015-10-08 22:52:40 +0800

    1

---
+++ mp/Extends/HtmlHelperExtend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+
+
+static public class HtmlHelperExtend
+{
+    static public MvcHtmlString MpDescription(this HtmlHelper helper, string description)
+    {
+        var regex = new Regex(@"#([^\s#]+?)#");
+        description = regex.Replace(description, (match) =>
+        {
+            var str = string.Format("<a class=\"mp-search-link\" href=\"/search/{0}\">{1}</a>", HttpUtility.UrlEncode(match.Groups[1].Value), match.Value);
+            return str;
+        });
+        return new MvcHtmlString(description);
+    }
+}
