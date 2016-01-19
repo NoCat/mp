f0c4f82f0864f21635e2c391e5bf5311561239d4
Hoody123 <Hoody_gs@qq.com>
2016-01-19 22:55:33 +0800

    2

--- BLL/OssFile.cs
+++ BLL/OssFile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Aliyun.OpenServices.OpenStorageService;
+using Aliyun.OSS;
 using System.IO;
 using System.Configuration;
 
