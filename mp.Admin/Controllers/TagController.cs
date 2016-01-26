dbb4c2fbaaffa06dd267c387adf0bf2c270c8141
Hoody123 <Hoody_gs@qq.com>
2016-01-26 23:04:06 +0800

    2

---
+++ mp.Admin/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mp.DAL;
+using mp.Admin.Models;
+
+namespace mp.Admin.Controllers
+{
+    public class TagController : ControllerBase
+    {
+        //
+        // GET: /Tag/
+
+        public ActionResult Index(int page=1)
+        {
+            var taglist = Manager.AdminPixivTags.Items.Where(i => (i.ID >= 40 * (page - 1) && i.ID < 40 * page)).ToList();
+            return View(taglist);
+        }
+
+        public ActionResult Edit(int id,string mtext)
+        {
+            var result = new AjaxResult();
+            var tag = Manager.AdminPixivTags.Find(id);
+            tag.MText = mtext;
+            Manager.AdminPixivTags.Update(tag);
+            return Content(Newtonsoft.Json.JsonConvert.SerializeObject(result),"application/json");            
+        }
+    }
+}
