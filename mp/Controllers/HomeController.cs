89291d8f472f4182ff3d138e06ce52ccb81ef007
Hoody123 <Hoody_gs@qq.com>
2016-02-18 18:21:18 +0800

    2

--- mp/Controllers/HomeController.cs
+++ mp/Controllers/HomeController.cs
@@ -50,6 +50,20 @@ namespace mp.Controllers
             return View("index.pc");
         }
 
+        public ActionResult Latest()
+        {
+            var imageList = new List<ImageInfo>();
+
+            Manager.Images.Items.Where(i => i.State == DAL.ImageStates.Ready).Take(40).ToList().ForEach(i =>
+            {
+                imageList.Add(new ImageInfo(i));
+            });
+
+            ViewBag.ImageList = imageList;
+
+            return View("Latest");
+            
+        }
 
         public ActionResult Test()
         {
