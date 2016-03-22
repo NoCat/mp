6c122ccf6d9cbac45c63a9c70cd5370754760f1a
nokitty <305870697@qq.com>
2016-03-22 17:04:35 +0800

    描述里面的网址变为超链接

--- mp/Extends/HtmlHelperExtend.cs
+++ mp/Extends/HtmlHelperExtend.cs
@@ -10,12 +10,20 @@ static public class HtmlHelperExtend
 {
     static public MvcHtmlString MpDescription(this HtmlHelper helper, string description)
     {
-        var regex = new Regex(@"#([^\s#]+?)#");
         if (description == null)
             description = "";
+        var regex = new Regex(@"#(.+?)#");
+
         description = regex.Replace(description, (match) =>
         {
             var str = string.Format("<a class=\"mp-search-link\" href=\"/search?kw={0}\">{1}</a>", HttpUtility.UrlEncode(match.Groups[1].Value), match.Value);
+            return str;
+        });
+
+        var regex2 = new Regex(@"(http|https):\/\/[A-Za-z0-9_\-_]+(\.[A-Za-z0-9_\-_]+)+([A-Za-z0-9_\-\.,@?^=%&amp;:/~\+#]*[A-Za-z0-9_\-\@?^=%&amp;/~\+#])?");
+        description = regex2.Replace(description, (match) =>
+        {
+            var str = string.Format("<a class=\"label label-default\" href=\"{0}\">网页链接 &gt;</a>", match.Value);
             return str;
         });
         return new MvcHtmlString(description);
